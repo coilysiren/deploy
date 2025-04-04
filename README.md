f33101e3de08baeeac991ccb6cf1822466ff29a0
kai [they] <coilysiren@gmail.com>
2025-04-03 22:26:58 -0700

    basic k8s cluster config

---
+++ README.md
@@ -0,0 +1,4 @@
+# coilysiren deployment platform co
+
+[Started with the Pulumi tutorial for GKE](https://www.pulumi.com/registry/packages/kubernetes/how-to-guides/gke/)
+
--- index.ts
+++ index.ts
@@ -1,1 +1,59 @@
+import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+const name = "coilysiren-deploy";
+
+// Create a GKE cluster
+const cluster = new gcp.container.Cluster(name, {
+  initialNodeCount: 1,
+  removeDefaultNodePool: true,
+  nodeConfig: {
+    machineType: "e2-standard-2",
+    oauthScopes: [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ],
+  },
+});
+
+// Export the Cluster name
+export const clusterName = cluster.name;
+
+// Manufacture a GKE-style kubeconfig. Note that this is slightly "different"
+// because of the way GKE requires gcloud to be in the picture for cluster
+// authentication (rather than using the client cert/key directly).
+export const kubeconfig = pulumi
+  .all([cluster.name, cluster.endpoint, cluster.masterAuth])
+  .apply(([name, endpoint, masterAuth]) => {
+    const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+    return `apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${masterAuth.clusterCaCertificate}
+    server: https://${endpoint}
+  name: ${context}
+contexts:
+- context:
+    cluster: ${context}
+    user: ${context}
+  name: ${context}
+current-context: ${context}
+kind: Config
+preferences: {}
+users:
+- name: ${context}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: gke-gcloud-auth-plugin
+      provideClusterInfo: true
+`;
+  });
+
+// Create a Kubernetes provider instance that uses our cluster from above.
+const clusterProvider = new k8s.Provider(name, {
+  kubeconfig: kubeconfig,
+});
